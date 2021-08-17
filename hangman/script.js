73cec57f63def4d2a176a794f3b52648d442b364
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-08-17 11:00:23 -0400

    logic for word

---
+++ hangman/script.js
@@ -0,0 +1,45 @@
+/** @format */
+
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letter");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll("figure-part");
+const words = [
+  "food",
+  "bob",
+  "nob",
+  "babganoosh",
+  "equestrian",
+  "post",
+  "guest",
+  "log",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `${selectedWord
+    .split("")
+    .map(
+      (letter) =>
+        `<span class="letter"> ${
+          correctLetters.includes(letter) ? letter : ""
+        } </span>`
+    )
+    .join("")}`;
+    const innerWord = wordEl.innerText.replace(/\n/g, '')
+    if(innerWord === selectedWord) {
+        finalMessage.innerText = 'CONGRATS! YOU WON!'
+        popup.style.display = 'flex'
+    }
+}
+
+displayWord();
