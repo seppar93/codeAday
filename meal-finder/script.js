7fe32ae0af4619188c805e23fe785baeb6202e1f
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-08-20 12:12:06 -0400

    my attempt

---
+++ meal-finder/script.js
@@ -0,0 +1,5 @@
+// API CALL www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+// random api www.themealdb.com/api/json/v1/1/random.php
+
+const searchBtn = document.getElementById('#search')
+const randomBtn = document.getElementById('#random')
